e5fa524e59aeca480e82820abd232ec6e6d534a2
Wvaviator <wvaviator@gmail.com>
2023-06-29 16:45:12 -0500

    add basic flat json return value for all nested properties

--- src/radixtree/mod.rs
+++ src/radixtree/mod.rs
@@ -1,9 +1,11 @@
-mod accesstype;
-mod radixnode;
-use self::{accesstype::AccessType, radixnode::RadixNode};
 use std::collections::HashMap;
 
-struct RadixTree {
+use self::{accesstype::AccessType, radixnode::RadixNode};
+
+mod accesstype;
+mod radixnode;
+
+pub struct RadixTree {
     root: RadixNode,
 }
 
@@ -14,8 +16,9 @@ impl RadixTree {
         }
     }
 
-    pub fn get(&self, key: &str) -> Vec<HashMap<String, String>> {
+    pub fn get(&self, key: &str) -> HashMap<String, String> {
         let access_type = AccessType::parse(key);
+        let mut results: HashMap<String, String> = HashMap::new();
 
         match access_type {
             AccessType::Direct => {
@@ -25,41 +28,62 @@ impl RadixTree {
                     let child = current.children.get(part);
                     match child {
                         Some(child) => current = child,
-                        None => return Vec::new(),
+                        None => return HashMap::new(),
                     }
                 }
-                let mut results = Vec::new();
+
                 if let Some(value) = &current.value {
-                    let mut result = HashMap::new();
-                    result.insert(key.to_string(), value.to_string());
-                    results.push(result);
+                    results.insert(key.to_string(), value.to_string());
                 }
                 results
             }
-            AccessType::Subtree(key) => {
+            AccessType::FullSubtree(key) => {
                 let mut current = &self.root;
                 let parts = key.split(".");
                 for part in parts {
                     let child = current.children.get(part);
                     match child {
                         Some(child) => current = child,
-                        None => return Vec::new(),
+                        None => return HashMap::new(),
                     }
                 }
                 let mut queue = Vec::new();
                 queue.push(current);
 
-                let mut results = Vec::new();
-
                 while queue.len() > 0 {
                     let node = queue.pop().unwrap();
                     if let Some(value) = &node.value {
-                        let mut result = HashMap::new();
-                        result.insert(node.key.to_string(), value.to_string());
-                        results.push(result);
+                        results.insert(node.key.to_string(), value.to_string());
                     }
                     for child in node.children.values() {
                         queue.push(child);
+                    }
+                }
+                results
+            }
+            AccessType::PartialSubtree(key, depth) => {
+                let mut current = &self.root;
+                let parts = key.split(".");
+                for part in parts {
+                    let child = current.children.get(part);
+                    match child {
+                        Some(child) => current = child,
+                        None => return HashMap::new(),
+                    }
+                }
+                let mut queue = Vec::new();
+                queue.push((current, 0));
+
+                while queue.len() > 0 {
+                    let (node, current_depth) = queue.pop().unwrap();
+                    if let Some(value) = &node.value {
+                        results.insert(node.key.to_string(), value.to_string());
+                    }
+                    if current_depth >= depth {
+                        continue;
+                    }
+                    for child in node.children.values() {
+                        queue.push((child, current_depth + 1));
                     }
                 }
                 results
@@ -82,25 +106,26 @@ impl RadixTree {
         current.value = Some(value);
     }
 
-    pub fn delete(&mut self, key: String) {
+    pub fn delete(&mut self, key: String) -> Option<String> {
         let mut current = &mut self.root;
         let parts = key.split(".");
         for part in parts {
             if current.children.contains_key(part) {
                 current = current.children.get_mut(part).unwrap();
             } else {
-                panic!("Key not found.")
+                return None;
             }
         }
 
+        let value = current.value.clone().unwrap();
         current.value = None;
+
+        Some(value)
     }
 }
 
 #[cfg(test)]
 mod test {
-    use crate::assert_vec_hashmap_eq;
-
     use super::*;
 
     #[test]
@@ -108,10 +133,7 @@ mod test {
         let mut radix = RadixTree::new();
         radix.put("key".to_string(), "value".to_string());
 
-        let expected = vec![HashMap::from_iter(vec![(
-            "key".to_string(),
-            "value".to_string(),
-        )])];
+        let expected = HashMap::from([("key".to_string(), "value".to_string())]);
         assert_eq!(radix.get("key"), expected);
     }
 
@@ -120,10 +142,7 @@ mod test {
         let mut radix = RadixTree::new();
         radix.put("key.abc.def".to_string(), "value".to_string());
 
-        let expected = vec![HashMap::from_iter(vec![(
-            "key.abc.def".to_string(),
-            "value".to_string(),
-        )])];
+        let expected = HashMap::from_iter([("key.abc.def".to_string(), "value".to_string())]);
 
         assert_eq!(radix.get("key.abc.def"), expected);
     }
@@ -135,15 +154,15 @@ mod test {
         radix.put("key.b".to_string(), "value2".to_string());
         radix.put("key.c".to_string(), "value3".to_string());
 
-        let expected: Vec<HashMap<String, String>> = vec![
-            HashMap::from_iter(vec![("key.a".to_string(), "value1".to_string())]),
-            HashMap::from_iter(vec![("key.b".to_string(), "value2".to_string())]),
-            HashMap::from_iter(vec![("key.c".to_string(), "value3".to_string())]),
-        ];
+        let expected = HashMap::from([
+            ("key.a".to_string(), "value1".to_string()),
+            ("key.b".to_string(), "value2".to_string()),
+            ("key.c".to_string(), "value3".to_string()),
+        ]);
 
         let actual = radix.get("key.*");
 
-        assert_vec_hashmap_eq!(actual, expected);
+        assert_eq!(actual, expected);
     }
 
     #[test]
@@ -153,14 +172,31 @@ mod test {
         radix.put("key.b".to_string(), "value2".to_string());
         radix.put("key.b.a".to_string(), "value3".to_string());
 
-        let expected: Vec<HashMap<String, String>> = vec![
-            HashMap::from_iter(vec![("key.a".to_string(), "value1".to_string())]),
-            HashMap::from_iter(vec![("key.b".to_string(), "value2".to_string())]),
-            HashMap::from_iter(vec![("key.b.a".to_string(), "value3".to_string())]),
-        ];
+        let expected = HashMap::from([
+            ("key.a".to_string(), "value1".to_string()),
+            ("key.b".to_string(), "value2".to_string()),
+            ("key.b.a".to_string(), "value3".to_string()),
+        ]);
 
         let actual = radix.get("key.*");
 
-        assert_vec_hashmap_eq!(actual, expected);
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn puts_and_gets_partial_subtree() {
+        let mut radix = RadixTree::new();
+        radix.put("key.a".to_string(), "value1".to_string());
+        radix.put("key.b".to_string(), "value2".to_string());
+        radix.put("key.b.a".to_string(), "value3".to_string());
+
+        let expected = HashMap::from([
+            ("key.a".to_string(), "value1".to_string()),
+            ("key.b".to_string(), "value2".to_string()),
+        ]);
+
+        let actual = radix.get("key.*1");
+
+        assert_eq!(actual, expected);
     }
 }
