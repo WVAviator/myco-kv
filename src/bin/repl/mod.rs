e21addecf9ec250e50b83f8597aeaf06618c70bb
Wvaviator <wvaviator@gmail.com>
2023-07-08 14:59:47 -0500

    add multithreaded long-lived connections

--- src/bin/repl/mod.rs
+++ src/bin/repl/mod.rs
@@ -1,4 +1,7 @@
-use std::io::{BufRead, BufReader};
+use std::{
+    io::{BufRead, BufReader},
+    net::TcpStream,
+};
 
 mod send;
 
@@ -7,12 +10,13 @@ pub fn start(port: u16) {
     let mut reader = BufReader::new(stdin);
 
     let addr = format!("localhost:{}", port);
+    let mut stream = TcpStream::connect(&addr).unwrap();
 
     loop {
         let mut buffer = String::new();
-        reader.read_line(&mut buffer).unwrap();
+        reader.read_line(&mut buffer).expect("Could not read from buffer");
 
-        match send::send_request(&addr, &buffer) {
+        match send::send_request(&mut stream, &buffer) {
             Ok(response) => {
                 println!("{}", response);
             }
