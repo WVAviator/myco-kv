991114645f128fb37090538723fd069c513332c0
Wvaviator <wvaviator@gmail.com>
2023-07-18 20:29:44 -0500

    add tests for value parsing

--- src/operation/value.rs
+++ src/operation/value.rs
@@ -41,3 +41,56 @@ impl Value {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    
+    use super::*;
+    
+    #[test]
+    fn parses_string() {
+        assert_eq!(Value::parse(r#""hello""#), Ok(Value::String("hello".to_string())));
+    }
+
+    #[test]
+    fn parses_integer() {
+        assert_eq!(Value::parse("123"), Ok(Value::Integer(123)));
+    }
+
+    #[test]
+    fn parses_float() {
+        assert_eq!(Value::parse("123.456"), Ok(Value::Float(123.456)));
+    }
+
+    #[test]
+    fn parses_boolean() {
+        assert_eq!(Value::parse("true"), Ok(Value::Boolean(true)));
+        assert_eq!(Value::parse("false"), Ok(Value::Boolean(false)));
+    }
+
+    #[test]
+    fn parses_null() {
+        assert_eq!(Value::parse("null"), Ok(Value::Null));
+    }
+
+    #[test]
+    fn parses_invalid_value() {
+        assert_eq!(Value::parse("hello"), Err(ParseError::InvalidValue("hello".to_string())));
+    }
+
+    #[test]
+    fn converts_string_to_string() {
+        assert_eq!("hello".to_string(), Value::String("hello".to_string()).to_string());
+    }
+
+    #[test]
+    fn converts_float_to_string() {
+        assert_eq!("123.45", Value::Float(123.45).to_string());
+    }
+
+    #[test]
+    fn converts_null_to_string() {
+        assert_eq!("null", Value::Null.to_string());
+    }
+
+}
