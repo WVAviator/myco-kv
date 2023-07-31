182a7223cd2a47bcd249886dbeb1a80c19f2e4ab
Wvaviator <wvaviator@gmail.com>
2023-07-30 20:26:17 -0500

    replace wal errors

--- src/wal/mod.rs
+++ src/wal/mod.rs
@@ -1,11 +1,8 @@
-use self::wal_error::WALError;
-use crate::operation::Operation;
+use crate::{errors::TransactionError, operation::Operation};
 use std::{
     fs::{File, OpenOptions},
     io::{BufRead, BufReader, Write},
 };
-
-mod wal_error;
 
 pub struct WriteAheadLog {
     file: File,
@@ -13,12 +10,12 @@ pub struct WriteAheadLog {
 }
 
 impl WriteAheadLog {
-    pub fn new(filename: &str) -> Result<WriteAheadLog, WALError> {
+    pub fn new(filename: &str) -> Result<WriteAheadLog, TransactionError> {
         let file = OpenOptions::new()
             .append(true)
             .create(true)
             .open(filename)
-            .map_err(|error| WALError::OpenError(error))?;
+            .map_err(|_| TransactionError::LogLoadFail)?;
 
         Ok(WriteAheadLog {
             file,
@@ -26,7 +23,7 @@ impl WriteAheadLog {
         })
     }
 
-    pub fn write(&mut self, operation: &Operation) -> Result<(), WALError> {
+    pub fn write(&mut self, operation: &Operation) -> Result<(), TransactionError> {
         let output = match operation {
             // Ignore get operations since they have no affect on db state
             Operation::Get(_) => return Ok(()),
@@ -41,29 +38,29 @@ impl WriteAheadLog {
 
         self.file
             .write_all(output.as_bytes())
-            .map_err(|error| WALError::WriteError(error))?;
+            .map_err(|error| TransactionError::LogWriteFail(error.to_string()))?;
 
         Ok(())
     }
 
     pub fn read_all_lines(
         &self,
-    ) -> Result<impl Iterator<Item = std::io::Result<String>>, WALError> {
+    ) -> Result<impl Iterator<Item = std::io::Result<String>>, TransactionError> {
         self.read_from(0)
     }
 
     pub fn read_from(
         &self,
         offset: usize,
-    ) -> Result<impl Iterator<Item = std::io::Result<String>>, WALError> {
-        let file = File::open(&self.filename).map_err(|error| WALError::OpenError(error))?;
+    ) -> Result<impl Iterator<Item = std::io::Result<String>>, TransactionError> {
+        let file = File::open(&self.filename).map_err(|_| TransactionError::LogLoadFail)?;
         let reader = BufReader::new(file);
         let lines = reader.lines().skip(offset);
         Ok(lines)
     }
 
-    pub fn clear(&mut self) -> Result<(), WALError> {
-        self.file = File::create(&self.filename).map_err(|error| WALError::OpenError(error))?;
+    pub fn clear(&mut self) -> Result<(), TransactionError> {
+        self.file = File::create(&self.filename).map_err(|_| TransactionError::LogLoadFail)?;
         Ok(())
     }
 }
